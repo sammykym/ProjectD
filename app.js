c4447c0141cb5dfacc8d30143bc447ff31dad945
Kym <sammykym@gmail.com>
2023-11-08 16:36:53 +0800

    1108

--- app.js
+++ app.js
@@ -1,91 +1,127 @@
+//先安裝node.js與以下模組(npm install)
+// npm install
+// npm install express
+// npm install body-parser
+// npm install express-session
+// npm install ejs
+// npm install mysql
+// 以 Express 建立 Web 伺服器
 var express = require("express");
-var cors = require("cors");
 var app = express();
 
-app.use( express.static("public")  );
-app.use( express.json() );
-app.use( express.urlencoded( {extended: true}) );
-app.use(cors());
+// 以 body-parser 模組協助 Express 解析表單與JSON資料
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var fs = require("fs");
-var dataFileName = "./data.json";
+// Web 伺服器的靜態檔案置於 public 資料夾
+app.use(express.static("public"));
 
-app.listen(3000);
+// 以 express-session 管理狀態資訊
+var session = require('express-session');
+app.use(session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// 指定 esj 為 Express 的畫面處理引擎
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/view');
+
+// 一切就緒，開始接受用戶端連線
+app.listen(8000);
 console.log("Web伺服器就緒，開始接受用戶端連線.");
 console.log("「Ctrl + C」可結束伺服器程式.");
 
+//設定SQL(需打開MAMP)
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    user: "root",
+    password: "root",
+    host: "localhost", // localhost, 127.0.0.1
+    port: 3306,
+    database: "bulletin_board"
+});
 
-app.get("/list", function (req, res) {
-	var data = fs.readFileSync(dataFileName);
-	var todoList = JSON.parse(data);
-    res.set('Content-type', 'application/json');
-	res.send( JSON.stringify(todoList) );
+//get方法 (SQL) 
+//取全部
+
+//app.get("/自訂名稱/自訂名稱", function (輸入, 輸出) {
+app.get("/message_board", function (req, res) {
+    // connection.query("SQL指令", [參數], function(錯誤訊息,輸出結果){})
+    connection.query(
+        "select * from news",
+        [],
+        function (error, result) {
+            res.send(JSON.stringify(result));
+        }
+    )
 })
 
-app.get("/item/:id", function (req, res) {
-	var data = fs.readFileSync(dataFileName);
-	var todoList = JSON.parse(data);
+//取單一物件 使用id
+//app.get("/自訂名稱/自訂名稱/:id", function (輸入, 輸出) {
+app.get("/message_board/:id", function (req, res) {
+    // connection.query("SQL指令 ?=參數", [參數1,參數2], function(錯誤訊息,輸出結果){})
+    connection.query(
+        "select * from news where newsId = ?",
+        [req.params.id],
+        //req.params指所有來源參數(:id)
+        function (error, result) {
+            if (result.length <= 0) {
+                res.send("{}")
 
-	var targetIndex = -1;
-	for (let i = 0; i < todoList.length; i++) {
-		if (todoList[i].tap_color_Id.toString() == req.params.id.toString()) {
-			targetIndex = i;
-			break;
-		}
-	}
-    if ( targetIndex < 0 ) {
-        res.send("not found");
-        return;
-    }
+            }
+            else {
+                res.send(JSON.stringify(result[0]));
+            }
 
-	res.set('Content-Type', 'application/json');
-    res.send( JSON.stringify(todoList[targetIndex]) );
+        }
+    )
+
 })
 
-// app.post("/create", function (req, res) {
-// 	var data = fs.readFileSync(dataFileName);
-// 	var todoList = JSON.parse(data);
-// 	var item = {
-// 		"tap_color_Id": new Date().getTime(),
-// 		"title": req.body.title,
-// 		"isComplete": req.body.isComplete
-// 	};
-// 	todoList.push(item);
-// 	fs.writeFileSync("./data.json", JSON.stringify(todoList, null, "\t"));
-// 	res.send("row inserted.");
-// })
-
-app.put("/item", function (req, res) {
-	var data = fs.readFileSync(dataFileName);
-	var todoList = JSON.parse(data);
-	for (let i = 0; i < todoList.length; i++) {
-		if (todoList[i].tap_color_Id == req.body.tap_color_Id) {
-			todoList[i].title = req.body.title;
-			todoList[i].isComplete = req.body.isComplete;
-			break;
-		}
-	}
-	fs.writeFileSync("./data.json", JSON.stringify(todoList, null, "\t"));	
-	res.send("row updated."); 
+app.post('/message_board', function (req, res) {
+    connection.query(
+        'insert into news(title,ymd) value (?,?)',
+        [req.body.title, req.body.ymd],
+        function (error, result) {
+            console.log(result)
+            res.send("OK, row(s) inserted")
+        }
+    )
 })
 
-app.delete("/delete/:id", function (req, res) {
-	var data = fs.readFileSync(dataFileName);
-	var todoList = JSON.parse(data);
+//修改
+app.put("/message_board", function (request, response) {
+    connection.query(
+        "update news set title = ?, ymd = ? where newsId = ?",
+        [
+            request.body.title,
+            request.body.ymd,
+            request.body.newsId
+        ]);
+    response.send("row updated.");
+})
 
-	var deleteIndex = -1;
-	for (let i = 0; i < todoList.length; i++) {
-		if (todoList[i].tap_color_Id.toString() == req.params.id.toString()) {
-			deleteIndex = i;
-			break;
-		}
-	}
-    if ( deleteIndex < 0 ) {
-        res.send("not found");
-        return;
-    }
-
-    todoList.splice(deleteIndex, 1);
-    fs.writeFileSync("./data.json", JSON.stringify(todoList, null, "\t"));	
+//刪除
+app.delete('message_board',function (req,res) {
+    connection.query(
+        "delete from news where newsId = ?",
+        [request.body.newsId]
+    );
     res.send("row deleted.");
 })
+
+
+
+
+//----使用ejs
+//安裝 npm install ejs
+//新增資料夾views 裡面新增index.ejs
+//index.ejs為模板 欲加入元素使用<%= 元素名稱 %>
+// app.get("/",function (req,res) {
+//     //載入模板(?)
+//     res.render("index.ejs", {userName:"chen"});
+// })
